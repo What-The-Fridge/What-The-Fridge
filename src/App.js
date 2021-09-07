44f3ca8a229b2742ea63e35aab1dfa703b311bc6
Chia-Sheng Lin <cl0204@student.ubc.ca>
2021-09-07 15:42:41 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. lalala
+          Edit <code>src/App.js</code> and save to reload. imagine day
+          
         </p>
         <a
           className="App-link"
