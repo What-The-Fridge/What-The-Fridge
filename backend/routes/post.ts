cda99afdc5e79f4c420ce3834e35600566945b47
Hung Nguyen <hunglnl01@gmail.com>
2021-09-29 23:57:09 -0700

    creating 2 new endpoints for user route and add status codes

--- backend/routes/post.ts
+++ backend/routes/post.ts
@@ -3,44 +3,47 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 router.get('/', (req: any, res: any) => {
-    res.send('We are on post');
+	res.send('We are on post');
 });
 
 // create a post
-router.post('/createpost', (req: any, res: any) => {
-    // console.log(req.body)
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description,
-        body: req.body.body,
-    })
+router.post('/createPost', (req: any, res: any) => {
+	// console.log(req.body)
+	const post = new Post({
+		title: req.body.title,
+		description: req.body.description,
+		body: req.body.body,
+	});
 
-    post.save().then((data: any) => {
-        res.json(data);
-    }).catch((err: any) => {
-        res.json({message: err})
-    });
-})
+	post
+		.save()
+		.then((data: any) => {
+			res.json(data);
+		})
+		.catch((err: any) => {
+			res.json({ message: err });
+		});
+});
 
-// get a specific post
-router.get('/getpost/:id', async (req: any, res: any) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        res.json(post);
-    } catch (err : any) {
-        res.json({message: err});
-    }
-})
+// get a specific post by id
+router.get('/getPost/:id', async (req: any, res: any) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		res.json(post);
+	} catch (err: any) {
+		res.json({ message: err });
+	}
+});
 
-// delete a specific post
-router.delete('/deletepost/:id', async (req: any, res: any) => {
-    try {
-        // deleteOne = remove
-        const deletePost = await Post.deleteOne({ _id : req.params.id });
-        res.json(deletePost);
-    } catch ( err: any ) {
-        res.json({message: err});
-    }
-})
+// delete a specific post by id
+router.delete('/deletePost/:id', async (req: any, res: any) => {
+	try {
+		// deleteOne = remove
+		const deletePost = await Post.deleteOne({ _id: req.params.id });
+		res.json(deletePost);
+	} catch (err: any) {
+		res.json({ message: err });
+	}
+});
 
 module.exports = router;
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { json } from 'stream/consumers';
 const router = express.Router();
 const User = require('../models/User');
 
 //create a user
-router.post('/createuser', (req: any, res: any) => {
+router.post('/createUser', (req: any, res: any) => {
 	// console.log(req.body)
 	const user = new User({
 		name: req.body.name,
@@ -20,6 +21,40 @@ router.post('/createuser', (req: any, res: any) => {
 		.catch((err: any) => {
 			res.json({ message: err });
 		});
+});
+
+//delete a user by id
+router.delete('/deleteUser/:id', async (req: any, res: any) => {
+	try {
+		const id = req.body.id;
+		if (!id) return res.status(404).send('bad input');
+
+		const deleteUser = await User.deleteOne({ _id: req.body.id });
+		if (deleteUser) {
+			res.status(200).json(deleteUser);
+		} else {
+			res.status(204).send('no such user to delete');
+		}
+	} catch (err: any) {
+		res.status(404).log({ message: err });
+	}
+});
+
+//get users by email or phone
+router.get('/byEmailOrPhone', async (req: any, res: any) => {
+	try {
+		const { email, phone } = req.query;
+		if (!email && !phone) return res.status(400).send('bad input');
+
+		const byEmailOrPhone = await User.findOne(email ? { email } : { phone });
+		if (byEmailOrPhone) {
+			return res.status(200).json(byEmailOrPhone);
+		} else {
+			return res.status(204);
+		}
+	} catch (err: any) {
+		res.status(404).log({ message: err });
+	}
 });
 
 module.exports = router;
