842781c9031128d4f42293ae8063f8a818c96c8b
Hung Nguyen <hunglnl01@gmail.com>
2021-10-09 23:54:19 -0700

    start off the android app

---
+++ android-app/app/src/main/java/com/project2021/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.project2021.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // hello world
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
