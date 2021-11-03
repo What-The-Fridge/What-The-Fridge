f9a8d36235bfe7c29b1c074807bac9fb87ad13d6
Hung Nguyen <hunglnl01@gmail.com>
2021-11-03 00:50:46 -0700

    register stuff

---
+++ what-the-fridge/src/components/InputField.tsx
@@ -0,0 +1,25 @@
+import { Input } from '@chakra-ui/input';
+import { FormControl, FormLabel, FormErrorMessage } from '@chakra-ui/react';
+import { useField } from 'formik';
+import React, { InputHTMLAttributes } from 'react';
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+	label: string;
+	placeholder: string;
+	name: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+	label,
+	size: _,
+	...props
+}) => {
+	const [field, { error }] = useField(props);
+	return (
+		<FormControl isInvalid={!!error}>
+			<FormLabel htmlFor={field.name}>{label}</FormLabel>
+			<Input {...field} {...props} id={field.name} />
+			{error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+		</FormControl>
+	);
+};
---
+++ what-the-fridge/src/components/Wrapper.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+interface WrapperProps {
+	variant?: 'small' | 'regular';
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({
+	children,
+	variant = 'regular',
+}) => {
+	return (
+		<Box
+			mt={8}
+			mx="auto"
+			maxW={variant === 'regular' ? '800px' : '400px'}
+			w="100%"
+		>
+			{children}
+		</Box>
+	);
+};
