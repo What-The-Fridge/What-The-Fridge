38e8362588844a96221950fb866b21ef1d6b2845
Hung Nguyen <hunglnl01@gmail.com>
2022-02-20 13:43:29 -0800

    api_url

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,7 +1,7 @@
 import { dedupExchange, cacheExchange, fetchExchange } from '@urql/core';
 
 export const createUrqlClient = (ssrExchange: any) => ({
-	url: 'http://localhost:4000/graphql',
+	url: process.env.NEXT_PUBLIC_API_URL,
 	fetchOptions: {
 		credentials: 'include' as const,
 		headers: {
