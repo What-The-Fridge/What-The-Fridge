15eb2d9cccf7aafa7d0781f0d1b54ee7f23827a8
Hung Nguyen <hunglnl01@gmail.com>
2022-02-25 20:38:59 -0800

    favicon & darkmode switcher

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,50 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
 
 export default class Document extends NextDocument {
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          {/* Make Color mode to persists when you refresh the page. */}
-          <ColorModeScript />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		return (
+			<Html>
+				{/* <a
+					href="https://www.flaticon.com/free-icons/fridge"
+					title="fridge icons"
+				>
+					Fridge icons created by Freepik - Flaticon
+				</a> */}
+				<Head>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/icons/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/icons/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/icons/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/icons/site.webmanifest" />
+					<link
+						rel="mask-icon"
+						href="/icons/safari-pinned-tab.svg"
+						color="#5bbad5"
+					/>
+					<meta name="msapplication-TileColor" content="#da532c" />
+					<meta name="theme-color" content="#ffffff" />
+				</Head>
+				<body>
+					{/* Make Color mode to persists when you refresh the page. */}
+					<ColorModeScript />
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
